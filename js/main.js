9689569e863ddcf6d7b744aec3db7d2db17bcbe3
Sean Kennedy <kenneds6@mail.uc.edu>
2017-06-08 10:05:07 -0400

    version 0.1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function topFunction() {
+    $('body,html').animate({scrollTop: 0}, 500);
+    //document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+
+}
+
+function scrollToAbout() {
+
+}
